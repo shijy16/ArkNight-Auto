1364e4dc970ddf265ae0c6af00db3f3e52c847cd
shijy16 <745921992@qq.com>
2019-06-11 14:37:31 +0800

    add no intelligence situation

--- run.py
+++ run.py
@@ -1,17 +1,19 @@
 import autopress
 import time
+import easygui
 
-pic_dirs = ['start_map.png','start_charac.png','running.png','finish.png']
+pic_dirs = ['start_map.png','no_intellect.png','start_charac.png','running.png','finish.png']
 start_map = 0
-start_charac = 1
-finish = 3
-running = 2
+no_intellect = 1
+start_charac = 2
+finish = 4
+running = 3
 
 next_state = 0
 
 while(True):
     flag = False
-    if(next_state == running):
+    if(next_state == running or next_state == no_intellect):
         flag = autopress.find_pos(pic_dirs[next_state],False)
     else:
         flag = autopress.find_pos(pic_dirs[next_state],True)
@@ -20,6 +22,10 @@ while(True):
         time.sleep(5)
         if(next_state == running):
             time.sleep(10)
+        if(next_state == no_intellect):
+            autopress.find_pos('cancel.png',True)
+            easygui.msgbox("理智耗尽，小助手将退出", title="警告",ok_button="ok") 
+            exit(0)
     next_state += 1
     if(next_state > 3):
         next_state = 0
